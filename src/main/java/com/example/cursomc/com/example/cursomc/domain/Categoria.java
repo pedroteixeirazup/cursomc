fc607228994787a1e7af1275b275d17c4207e605
Pedro <pedro.teixeira@zup.com.br>
2018-08-21 16:08:04 -0300

    Banco de dados H2 e criação automatica da base de bados

--- src/main/java/com/example/cursomc/com/example/cursomc/domain/Categoria.java
+++ src/main/java/com/example/cursomc/com/example/cursomc/domain/Categoria.java
@@ -1,11 +1,18 @@
 package com.example.cursomc.com.example.cursomc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id //chave primaria no banco
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private  String nome;
 
