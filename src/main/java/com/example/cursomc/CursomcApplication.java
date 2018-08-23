b97b2c6e6470a48c1e7cf81316f1ba941a7dde6c
Pedro <pedro.teixeira@zup.com.br>
2018-08-23 14:39:01 -0300

    Cliente, TipoCliente, Telefones e Endereco

--- src/main/java/com/example/cursomc/CursomcApplication.java
+++ src/main/java/com/example/cursomc/CursomcApplication.java
@@ -1,13 +1,8 @@
 package com.example.cursomc;
 
-import com.example.cursomc.com.example.cursomc.com.example.cursomc.repositories.CategoriaRepository;
-import com.example.cursomc.com.example.cursomc.com.example.cursomc.repositories.CidadeRepository;
-import com.example.cursomc.com.example.cursomc.com.example.cursomc.repositories.EstadoRepository;
-import com.example.cursomc.com.example.cursomc.com.example.cursomc.repositories.ProdutoRepository;
-import com.example.cursomc.com.example.cursomc.domain.Categoria;
-import com.example.cursomc.com.example.cursomc.domain.Cidade;
-import com.example.cursomc.com.example.cursomc.domain.Estado;
-import com.example.cursomc.com.example.cursomc.domain.Produto;
+import com.example.cursomc.com.example.cursomc.com.example.cursomc.repositories.*;
+import com.example.cursomc.com.example.cursomc.domain.*;
+import com.example.cursomc.com.example.cursomc.domain.com.example.cursomc.domain.enums.TipoCliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -31,6 +26,11 @@ public class CursomcApplication implements CommandLineRunner {
 	@Autowired
     private CidadeRepository cidadeRepository;
 
+	@Autowired
+    private ClienteRepository clienteRepository;
+
+	@Autowired
+    private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -68,6 +68,17 @@ public class CursomcApplication implements CommandLineRunner {
 
         estadoRepository.saveAll(Arrays.asList(est1,est2));
         cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
-        
+
+        Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","36378912377", TipoCliente.PESSOAFISICA);
+
+        cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
+
+        Endereco e1 = new Endereco(null,"Rua Flores","300","Apto 303","Jardim","38220834",cli1,c1);
+        Endereco e2 = new Endereco(null,"Avenida Matos","105","Sala 800","Centro","38777012",cli1,c2);
+
+        cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+
+        clienteRepository.saveAll(Arrays.asList(cli1));
+        enderecoRepository.saveAll(Arrays.asList(e1,e2));
 	}
 }
