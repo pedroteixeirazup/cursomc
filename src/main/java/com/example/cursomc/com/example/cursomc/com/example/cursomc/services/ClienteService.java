3ddb635dfbb6cca370f303651e6e04bfcc6b0e9e
Pedro <pedro.teixeira@zup.com.br>
2018-09-21 14:57:52 -0300

    Inserindo um novo cliente com Post.

--- src/main/java/com/example/cursomc/com/example/cursomc/com/example/cursomc/services/ClienteService.java
+++ src/main/java/com/example/cursomc/com/example/cursomc/com/example/cursomc/services/ClienteService.java
@@ -1,11 +1,17 @@
 package com.example.cursomc.com.example.cursomc.com.example.cursomc.services;
 
+import com.example.cursomc.com.example.cursomc.com.example.cursomc.repositories.CidadeRepository;
 import com.example.cursomc.com.example.cursomc.com.example.cursomc.repositories.ClienteRepository;
+import com.example.cursomc.com.example.cursomc.com.example.cursomc.repositories.EnderecoRepository;
 import com.example.cursomc.com.example.cursomc.com.example.cursomc.services.com.example.cursomc.services.exceptions.DataIntegityException;
 import com.example.cursomc.com.example.cursomc.com.example.cursomc.services.com.example.cursomc.services.exceptions.ObjectNotFoundExcepetion;
+import com.example.cursomc.com.example.cursomc.domain.Cidade;
 import com.example.cursomc.com.example.cursomc.domain.Cliente;
 import com.example.cursomc.com.example.cursomc.domain.Cliente;
+import com.example.cursomc.com.example.cursomc.domain.Endereco;
+import com.example.cursomc.com.example.cursomc.domain.com.example.cursomc.domain.enums.TipoCliente;
 import com.example.cursomc.com.example.cursomc.dto.ClienteDTO;
+import com.example.cursomc.com.example.cursomc.dto.ClienteNewDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -13,6 +19,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +29,8 @@ public class ClienteService {
     @Autowired
     private ClienteRepository repo;
 
+    @Autowired
+    private EnderecoRepository enderecoRepository;
 
         public Cliente find(Integer id) {
             Optional<Cliente> obj = repo.findById(id);
@@ -34,6 +43,14 @@ public class ClienteService {
         Cliente newObj = find(obj.getId());
         updateData(newObj,obj);
         return repo.save(newObj);
+   }
+
+    @Transactional
+    public Cliente insert(Cliente obj){
+            obj.setId(null);
+            obj = repo.save(obj);
+            enderecoRepository.saveAll(obj.getEnderecos());
+            return obj;
     }
 
     public void delete(Integer id){
@@ -59,6 +76,29 @@ public class ClienteService {
     public Cliente fromDTO(ClienteDTO objDTO){
 
         return new Cliente(objDTO.getId(),objDTO.getNome(),objDTO.getEmail(),null,null);
+    }
+
+    public Cliente fromDTO(ClienteNewDTO objDTO){
+
+            Cliente cli = new Cliente(null,objDTO.getNome(),objDTO.getEmail(),objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+
+            Cidade cid = new Cidade(objDTO.getCidadeId(),null,null);
+            Endereco end = new Endereco(null,objDTO.getLogradouro(),objDTO.getNumero(),objDTO.getComplemento(),objDTO
+            .getBairro(),objDTO.getCep(),cli,cid);
+
+            cli.getEnderecos().add(end);
+            cli.getTelefones().add(objDTO.getTelefone1());
+
+            if(objDTO.getTelefone2()!=null){
+
+                cli.getTelefones().add(objDTO.getTelefone2());
+            }
+        if(objDTO.getTelefone2()!=null){
+
+            cli.getTelefones().add(objDTO.getTelefone3());
+        }
+
+        return cli;
     }
 
     private void updateData(Cliente newObj,Cliente obj){
--- src/main/java/com/example/cursomc/com/example/cursomc/resource/ClienteResource.java
+++ src/main/java/com/example/cursomc/com/example/cursomc/resource/ClienteResource.java
@@ -3,13 +3,17 @@ package com.example.cursomc.com.example.cursomc.resource;
 import com.example.cursomc.com.example.cursomc.com.example.cursomc.services.ClienteService;
 import com.example.cursomc.com.example.cursomc.domain.Cliente;
 import com.example.cursomc.com.example.cursomc.domain.Cliente;
+import com.example.cursomc.com.example.cursomc.domain.Cliente;
+import com.example.cursomc.com.example.cursomc.dto.ClienteDTO;
 import com.example.cursomc.com.example.cursomc.dto.ClienteDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,6 +30,15 @@ public class ClienteResource {
         return ResponseEntity.ok().body(obj);
     }
 
+    @RequestMapping(method = RequestMethod.POST) //Insercao do CRUD
+    public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDto){
+
+        Cliente obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
 
     @RequestMapping(value = "/{id}",method = RequestMethod.PUT)//Atualizacao do CRUD
     public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
