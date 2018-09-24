aefa4641450108cf067af9a1b720fc3a5bcb9197
Pedro <pedro.teixeira@zup.com.br>
2018-09-24 14:37:56 -0300

    Validação customizada, email não repetido na inserção de cliente.

--- src/main/java/com/example/cursomc/com/example/cursomc/com/example/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/example/cursomc/com/example/cursomc/com/example/cursomc/repositories/ClienteRepository.java
@@ -3,9 +3,12 @@ package com.example.cursomc.com.example.cursomc.com.example.cursomc.repositories
 import com.example.cursomc.com.example.cursomc.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email); // o spring data cria sozinho a busca por email.
 
 }
--- src/main/java/com/example/cursomc/com/example/cursomc/com/example/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/example/cursomc/com/example/cursomc/com/example/cursomc/services/validation/ClienteInsertValidator.java
@@ -1,16 +1,23 @@
 package com.example.cursomc.com.example.cursomc.com.example.cursomc.services.validation;
 
 import ch.qos.logback.core.net.server.Client;
+import com.example.cursomc.com.example.cursomc.com.example.cursomc.repositories.ClienteRepository;
 import com.example.cursomc.com.example.cursomc.com.example.cursomc.services.validation.utils.BR;
+import com.example.cursomc.com.example.cursomc.domain.Cliente;
 import com.example.cursomc.com.example.cursomc.domain.com.example.cursomc.domain.enums.TipoCliente;
 import com.example.cursomc.com.example.cursomc.dto.ClienteNewDTO;
 import com.example.cursomc.com.example.cursomc.resource.com.excample.cursomc.resource.exception.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO>
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
     @Override
     public void initialize(ClienteInsert ann) {
     }
@@ -25,7 +32,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
             list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido."));
         }
-        // inclua os testes aqui, inserindo erros na lista
+
+        Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+
+        if(aux != null) {
+
+            list.add(new FieldMessage("email","Email já existente."));
+        }
 
         for (FieldMessage e : list) {
             context.disableDefaultConstraintViolation();
