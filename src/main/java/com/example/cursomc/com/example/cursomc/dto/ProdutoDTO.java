7d560480addba757045d018ab1c51166d3ad9c26
Pedro <pedro.teixeira@zup.com.br>
2018-09-25 14:13:12 -0300

    Sub total e subtotal do Pedido.

--- src/main/java/com/example/cursomc/com/example/cursomc/dto/ProdutoDTO.java
+++ src/main/java/com/example/cursomc/com/example/cursomc/dto/ProdutoDTO.java
@@ -3,7 +3,7 @@ package com.example.cursomc.com.example.cursomc.dto;
 import com.example.cursomc.com.example.cursomc.domain.Produto;
 
 import java.io.Serializable;
-//Deu certo 
+//Deu certo
 public class ProdutoDTO implements Serializable {
     private static final long serialVersionUID = 1L;
     private Integer id;
